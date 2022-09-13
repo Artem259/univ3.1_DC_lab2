54238e4ad0f87dc87ab90d274eadb5728693b952
Artem Poliakov <artem4250@gmail.com>
2022-09-14 02:19:34 +0300

    Forest class update

--- task_a/Forest.java
+++ task_a/Forest.java
@@ -1,11 +1,13 @@
 public class Forest {
-    boolean[][] forest;
+    private final Integer[][] forest;
+    private volatile boolean isBearFound;
 
     public Forest(int rows, int columns) {
-        forest = new boolean[rows][columns];
+        isBearFound = false;
+        forest = new Integer[rows][columns];
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < columns; j++) {
-                forest[i][j] = false;
+                forest[i][j] = 0;
             }
         }
     }
@@ -19,10 +21,18 @@ public class Forest {
     }
 
     public void setBear(int row, int column) {
-        forest[row][column] = true;
+        forest[row][column] = 1;
     }
 
     public boolean checkBear(int row, int column) {
-        return forest[row][column];
+        if(forest[row][column] == 1) {
+            isBearFound = true;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isBearFound() {
+        return isBearFound;
     }
 }
